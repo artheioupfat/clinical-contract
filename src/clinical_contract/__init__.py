9cd78e90faac6d3cb35e62611c06c1b7dd774837
artheioupfat <arthur.prigent@chu-brest.fr>
2026-03-18 16:04:59 +0100

    git add .github

--- src/clinical_contract/__init__.py
+++ src/clinical_contract/__init__.py
@@ -51,4 +51,8 @@ __all__ = [
     "auto_backend",
 ]
 
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("clinical-contract")
+except PackageNotFoundError:
+    __version__ = "unknown"
