8de912d8659c496531ca3e59064fbbc9ed82dec8
artheioupfat <arthurprigent760@gmail.com>
2026-08-04 14:40:24 +0200

    chore(release): prepare v0.2.0

--- site/js/site-version.js
+++ site/js/site-version.js
@@ -1,7 +1,7 @@
 (function registerSiteVersion(root) {
 // Generated from pyproject.toml. Do not edit by hand.
 // SITE_VERSION_JSON_START
-const version = "0.1.9";
+const version = "0.2.0";
 // SITE_VERSION_JSON_END
 
 root.ClinicalContractVersion = version;
